aac5f2cc0361984fb260a380d9b714cc3766341c
erdemgocen44 <erdemgocen44@gmail.com>
2022-06-22 14:57:25 +0200

    soru sayfası başlandı

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/questionwidget.dart';
 
 void main() => runApp(QuizApp());
 
 class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Quiz App',
       home: Scaffold(
         body: QuestionWidget(),
--- lib/questionwidget.dart
+++ lib/questionwidget.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/model.dart';
 
 class QuestionWidget extends StatefulWidget {
   const QuestionWidget({Key? key}) : super(key: key);
-
   @override
   State<QuestionWidget> createState() => _QuestionWidgetState();
 }
+
+int _questionNumber = 1;
 
 class _QuestionWidgetState extends State<QuestionWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: []),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.1,
+        ),
+        Text('Soru $_questionNumber/${questions.length}'),
+        const Divider(thickness: 1, color: Colors.grey),
+      ]),
     );
   }
 }
