baba906cd7b57dc477ccb7b9c61e57a0dfd0421e
erdemgocen44 <erdemgocen44@gmail.com>
2022-06-24 12:24:33 +0200

    build option container

--- lib/build_option.dart
+++ lib/build_option.dart
@@ -2,19 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:quiz/model.dart';
 
 Widget buildOption(BuildContext context, Option option) {
-  return Container(
-    height: MediaQuery.of(context).size.height * 0.07,
-    padding: const EdgeInsets.all(12),
-    margin: const EdgeInsets.symmetric(vertical: 8),
-    decoration: BoxDecoration(color: Colors.grey.shade200),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          option.text,
-          style: const TextStyle(fontSize: 20),
-        )
-      ],
+  return GestureDetector(
+    child: Container(
+      height: MediaQuery.of(context).size.height * 0.07,
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            option.text,
+            style: const TextStyle(fontSize: 20),
+          )
+        ],
+      ),
     ),
   );
 }
