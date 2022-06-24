8935640e920e4d9d41b1a576c2c03ee8797d5ec0
erdemgocen44 <erdemgocen44@gmail.com>
2022-06-24 12:09:56 +0200

    build option container

--- lib/build_option.dart
+++ lib/build_option.dart
@@ -3,7 +3,8 @@ import 'package:quiz/model.dart';
 
 Widget buildOption(BuildContext context, Option option) {
   return Container(
-    height: MediaQuery.of(context).size.height * 0.1,
+    height: MediaQuery.of(context).size.height * 0.07,
+    padding: const EdgeInsets.all(12),
     decoration: BoxDecoration(color: Colors.grey.shade200),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
